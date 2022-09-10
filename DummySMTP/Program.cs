63e02518217d35e111bf537b9a742890b9c19888
mail@fort-hub.com <mail@fort-hub.com>
2022-09-11 00:49:49 +0100

    Added a quick SMTP client to test the server. Got futher with StartTLS support - jus need to get the SslStream auth working

--- DummySMTP/Program.cs
+++ DummySMTP/Program.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Mail;
+using System.Net.Security;
 using System.Net.Sockets;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -21,6 +24,7 @@ namespace DummySMTP
         byte[] _buffer;
         TcpListener _server;
         List<string> _messages = new List<string>();
+        SslStream _secureStream;
 
         public SmtpServer(SmtpServerConfig config)
         {
@@ -48,12 +52,13 @@ namespace DummySMTP
             if (client.Connected)
             {
                 Console.WriteLine("Connected to client");
-                using (NetworkStream stream = client.GetStream())
+                using (Stream stream = client.GetStream())
+                using(_secureStream = new SslStream(stream))
                 {
-                    string ack = FrameMessage("220 test.com ESMTP Exim");
+                    string ack = FrameMessage("220 Ready");
                     Console.WriteLine($"Sending to client: {ack}");
                     stream.Write(Encoding.ASCII.GetBytes(ack), 0, ack.Length);
-                    _buffer = new byte[1000];
+                    _buffer = new byte[10000];
                     Receive(stream);
                 }
             }
@@ -65,26 +70,58 @@ namespace DummySMTP
 
         const byte CR = 0x0D, LF = 0x0A;
 
-        private void Receive(NetworkStream stream, int offset = 0)
+        private void Receive(Stream stream)
         {
-            stream.Read(_buffer, offset, 1);
+            int offset = 0;
 
-            if(offset > 0 && _buffer[offset-1] == CR && _buffer[offset] == LF)
+            while (true)
             {
-                string message = Encoding.ASCII.GetString(_buffer.Take(offset).ToArray());
-                _messages.Add(message);
-                Console.WriteLine(message);
-                byte[] response = Encoding.ASCII.GetBytes(FrameMessage(GetResponse(message)));
-                if(stream == null) // Need to check socket connected, pass that in instead of stream?
-                {
-                    Console.WriteLine("Connection ended");
-                    return;
-                }
-                stream.Write(response, 0, response.Length);
-                Receive(stream);
-            }
+                stream.Read(_buffer, offset, 1);
 
-            Receive(stream, ++offset);
+                if (offset > 0 && _buffer[offset - 1] == CR && _buffer[offset] == LF)
+                {
+                    string message = Encoding.UTF8.GetString(_buffer.Take(offset).ToArray());
+                    _messages.Add(message);
+                    Console.WriteLine(message);
+
+                    string[] payload = GetResponsePayload(message);
+
+                    List<byte[]> data = new List<byte[]>();
+
+                    foreach (string response in payload)
+                    {
+                        byte[] buffer = Encoding.ASCII.GetBytes(FrameMessage(response));
+
+                        Console.WriteLine($"Sending: {response}");
+                        stream.Write(buffer, 0, buffer.Length);
+                    }
+
+                    if (message == "STARTTLS\r")
+                    {
+                        X509Store certStore = new X509Store(StoreName.My, StoreLocation.LocalMachine);
+                        certStore.Open(OpenFlags.ReadOnly);
+                        X509Certificate2 cert = certStore.Certificates.Find(X509FindType.FindBySerialNumber, "1567752d2866598749220a1c712f944e", true)[0];
+                        try
+                        {
+                            _secureStream.AuthenticateAsServer(cert, false, false);
+                            if(!_secureStream.IsAuthenticated)
+                            {
+                                return;
+                            }
+                        }
+                        catch (Exception e)
+                        {
+                            return;
+                        }
+                    }
+
+                    offset = 0;
+                    _buffer = new byte[10000];
+                    continue;
+                }
+
+                ++offset;
+            }
         }
 
         private string Sanitize(string message)
@@ -92,40 +129,46 @@ namespace DummySMTP
             return new string(message.Where(x => !new char[] { '\r', '\n' }.Contains(x)).ToArray());
         }
 
-        private string GetResponse(string message)
+        private string[] GetResponsePayload(string message)
         {
             IEnumerable<string> parts = Sanitize(message).Split(' ');
             string cmd = parts.First();
-            string response = "";
+            string[] response;
 
             switch (cmd)
             {
                 case "HELO":
                 case "EHLO":
-                    response = string.Format(HelloResponse, parts.ElementAt(1));
+                    response = new string[] { string.Format(HelloResponse, parts.ElementAt(1)), TLSResponse, OKResponse };
                     break;
                 case "MAIL":
-                    response = OKResponse;
+                    response = new string[] { OKResponse };
                     break;
                 case "RCPT":
-                    response = AcceptedResponse;
+                    response = new string[] { AcceptedResponse };
                     break;
                 case "DATA":
-                    response = DataResponse;
+                    response = new string[] { DataResponse };
+                    break;
+                case "STARTTLS":
+                    response = new string[] { StartTLSResponse };
                     break;
                 default:
-                    response = OKResponse;
+                    response = new string[] { OKResponse };
                     break;
             }
 
             return response;
         }
 
-        private string HelloResponse => "250 Hello {0}";
+        //private string HelloResponse => "250 OK";
+        private string HelloResponse => "250-server.test.com Hello {0}";
+        private string TLSResponse => "250-STARTTLS";
         private string OKResponse => "250 OK";
         private string AcceptedResponse => "250 Accepted";
         private string DataResponse => "354 Enter message, ending with \".\" on a line by itself";
         private string QuitResponse => "quit";
+        private string StartTLSResponse => "220 2.0.0 SMTP server ready";
     }
 
     class Program
---
+++ DummySMTPClient/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DummySMTPClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting up...");
+            using (SmtpClient client = new SmtpClient("127.0.0.1", 25))
+            {
+                client.EnableSsl = true;
+                client.Send("test@from.com", "test@to.com", "asubject", "abody");
+            }
+        }
+    }
+}
